74a7147aae0cc1a754ce3976c26f52d2d10701e7
unitrixteam <106687041+unitrixteam@users.noreply.github.com>
2022-06-02 01:11:41 -0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,11 +1,34 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# .# Unitrix - UserBot
+# Copyright (C) 2021-2022 UnitrixTeam
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/unitrixteam/>
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# https://github.com/unitrixteam/unitrixuserbot/blob/main/LICENSE<>.
 
 
-from plugins import *
+from pyUltroid import *
+from pyUltroid import _ult_cache
+from pyUltroid._misc import owner_and_sudos
+from pyUltroid._misc._assistant import asst_cmd, callback, in_pattern
+from pyUltroid.functions.helper import *
+from pyUltroid.functions.tools import get_stored_file
+from telethon import Button, custom
 
-bot = ultroid_bot
+from plugins import ATRA_COL
+from strings import get_languages, get_string, language
+
+OWNER_NAME = ultroid_bot.full_name
+OWNER_ID = ultroid_bot.uid
+
+AST_PLUGINS = {}
+
+
+async def setit(event, name, value):
+    try:
+        udB.set_key(name, value)
+    except BaseException:
+        return await event.edit("`Something Went Wrong`")
+
+
+def get_back_button(name):
+    return [Button.inline("« Bᴀᴄᴋ", data=f"{name}")]
